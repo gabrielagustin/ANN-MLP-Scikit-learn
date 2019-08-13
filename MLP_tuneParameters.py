932fd4bb3ed0dfb67590585069e03dd921986d31
gabrielagustin <gabiagus@gmail.com>
2019-08-13 12:27:52 -0300

    improving code

--- MLP_tuneParameters.py
+++ MLP_tuneParameters.py
@@ -13,6 +13,11 @@ learning, penalty , among others. Samples of parameters:
 
                 'hidden_layer_sizes': [i for i in itertools.product(range(3,9),repeat=2)]
                 'solver': ['lbfgs', 'adam', sgd'],
+                The solver are:
+                - 'lbfgs' is an optimizer in the family of quasi-Newton methods.
+                - 'sgd' refers to stochastic gradient descent.
+                - 'adam' refers to a stochastic gradient-based optimizer proposed by Kingma, Diederik, and Jimmy Ba
+
                 'activation': ['relu', 'tanh', 'logistic'],
                 'learning_rate_init':np.linspace(0.001, 0.1, num=5),
                 'power_t': [0.5],
@@ -20,7 +25,9 @@ learning, penalty , among others. Samples of parameters:
                 'max_iter': [1000],
                 'early_stopping': [False],
                 'warm_start': [False],
-                'random_state': [9]
+                'random_state': [9
+
+Each solver method has its set of parameters so it separates them. 
 
 To carry out this search in the space of hyperparameters, optimization methods are used, 
 those tested in this script are:
@@ -43,13 +50,9 @@ import statsmodels.formula.api as smf
 
 
 def tune_Grid_Search(xTraining,yTraining, xTest, yTest, solverType):
-    """-------------------------------------------------------------------------------------------------
+    """
     Exhaustive Grid Search
-    Each solver method has its set of parameters so it separates them. The solver are:
-        - 'lbfgs' is an optimizer in the family of quasi-Newton methods.
-        - 'sgd' refers to stochastic gradient descent.
-        - 'adam' refers to a stochastic gradient-based optimizer proposed by Kingma, Diederik, and Jimmy Ba
-    """------------------------------------------------------------------------------------------------
+    """
     mlp = MLPRegressor()
     ### solver 'lbfgs'
     if(solverType == 'lbfgs'):
@@ -63,6 +66,7 @@ def tune_Grid_Search(xTraining,yTraining, xTest, yTest, solverType):
                         'warm_start': [False],
                         'random_state': [9]
                         }
+
     ### solver 'adam'
     if(solverType == 'adam'):
         param_grid = {'hidden_layer_sizes': [i for i in itertools.product(range(3,9),repeat=2)],
@@ -76,24 +80,25 @@ def tune_Grid_Search(xTraining,yTraining, xTest, yTest, solverType):
                         'warm_start': [False],
                         'random_state': [9]
                         }
-    #  {'warm_start': False, 'hidden_layer_sizes': (2, 3), 'activation': 'relu', 'max_iter': 1000, 'power_t': 0.5, 'random_state': 9, 'early_stopping': False, 'alpha': 0.001, 'solver': 'adam', 'learning_rate_init': 0.025750000000000002}
+
     ### solver 'sgd'
-    param_grid = {'hidden_layer_sizes': [i for i in itertools.product(range(3,9),repeat=2)],
-#                  'activation': ['relu', 'tanh', 'logistic'],
-                  'activation': ['logistic'],
-                  'solver': ['sgd'],
-#                  'learning_rate': ['constant', 'invscaling', 'adaptive'],
-                  'learning_rate': ['constant'],
-                  'learning_rate_init':np.linspace(0.001, 0.1, num=5),
-                  'power_t': [0.5],
-                  'alpha': np.linspace(0.001, 0.1, num=5),
-                  'momentum': np.linspace(0.1, 0.9, num=5), # cuando se usa 'sgd'
-                  'max_iter': [100],
-                  'early_stopping': [True],
-                  'validation_fraction': [0.25],
-                  'warm_start': [False],
-                  'random_state': [9]
-                  }
+    if(solverType == 'sgd'):
+        param_grid = {'hidden_layer_sizes': [i for i in itertools.product(range(3,9),repeat=2)],
+                    'activation': ['relu', 'tanh', 'logistic'],
+                    # 'activation': ['logistic'],
+                    'solver': ['sgd'],
+                    'learning_rate': ['constant', 'invscaling', 'adaptive'],
+                    # 'learning_rate': ['constant'],
+                    'learning_rate_init':np.linspace(0.001, 0.1, num=5),
+                    'power_t': [0.5],
+                    'alpha': np.linspace(0.001, 0.1, num=5),
+                    'momentum': np.linspace(0.1, 0.9, num=5), # cuando se usa 'sgd'
+                    'max_iter': [100],
+                    'early_stopping': [True],
+                    'validation_fraction': [0.25],
+                    'warm_start': [False],
+                    'random_state': [9]
+                    }
 
     Scoring='neg_mean_squared_error' 
     _GS = GridSearchCV(mlp, param_grid=param_grid, scoring=Scoring,
@@ -104,9 +109,36 @@ def tune_Grid_Search(xTraining,yTraining, xTest, yTest, solverType):
     print(_GS.best_score_)
     print(_GS.best_params_)
 
-########-------------------------------------------------------------------------------------------------
-######## Randomized Search
-########-------------------------------------------------------------------------------------------------
+    print("Calibracion")
+    y_pred_Cal = reg.predict(xTraining)
+#    print(y_pred_Cal)
+    y_true_Cal = np.array(yTraining)
+#    print(y_true_Cal)
+    rmse = statistics.RMSE(y_true_Cal, y_pred_Cal)
+    print("RMSE Cal:" + str(rmse))
+    data2 = pd.DataFrame({'yTest_SMAP' :y_true_Cal,'yAprox' :y_pred_Cal.T})
+    RR = smf.ols('yTest_SMAP ~ 1+ yAprox', data2).fit().rsquared
+    print("R^2 Cal: "+str(RR))
+
+
+    print("Validacion")
+    y_pred = reg.predict(xTest)
+    y_true = np.array(yTest)
+    rmse = statistics.RMSE(y_true, y_pred)
+    print("RMSE Val:" + str(rmse))
+    data2 = pd.DataFrame({'yTest_SMAP' :y_true,'yAprox' :y_pred.T})
+    RR = smf.ols('yTest_SMAP ~ 1+ yAprox', data2).fit().rsquared
+    print("R^2 Val: "+str(RR))
+
+    return reg, np.array(y_pred_Cal), np.array(y_pred)
+    
+
+
+
+def tune_Randomized_Search(xTraining,yTraining, xTest, yTest, solverType):
+    """
+    Randomized Parameter Optimization
+    """
 
 #    print("Metodo de tune: Randomized Search")
     #### solver 'lbfgs'
@@ -176,28 +208,3 @@ def tune_Grid_Search(xTraining,yTraining, xTest, yTest, solverType):
 #    reg = rs.fit(xTraining,yTraining)
 #    print(rs.best_score_)
 #    print(rs.best_params_)
-
-#    print("Calibracion")
-
-    y_pred_Cal = reg.predict(xTraining)
-#    print(y_pred_Cal)
-    y_true_Cal = np.array(yTraining)
-#    print(y_true_Cal)
-    rmse = statistics.RMSE(y_true_Cal, y_pred_Cal)
-    print("RMSE Cal:" + str(rmse))
-    data2 = pd.DataFrame({'yTest_SMAP' :y_true_Cal,'yAprox' :y_pred_Cal.T})
-    RR = smf.ols('yTest_SMAP ~ 1+ yAprox', data2).fit().rsquared
-    print("R^2 Cal: "+str(RR))
-
-
-    print("Validacion")
-    y_pred = reg.predict(xTest)
-    y_true = np.array(yTest)
-    rmse = statistics.RMSE(y_true, y_pred)
-    print("RMSE Val:" + str(rmse))
-    data2 = pd.DataFrame({'yTest_SMAP' :y_true,'yAprox' :y_pred.T})
-    RR = smf.ols('yTest_SMAP ~ 1+ yAprox', data2).fit().rsquared
-    print("R^2 Val: "+str(RR))
-
-    return reg, np.array(y_pred_Cal), np.array(y_pred)
-    
